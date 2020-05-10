51264b948fe341058a199168ca0d9fc70eb8df2a
Liam elliott <liam.elliott@gmail.com>
2020-05-10 20:57:07 +1000

    Removed home.css, added tailwind to layout.css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Link } from "gatsby"
-import "./home.css"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
